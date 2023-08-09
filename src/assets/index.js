763fb90bd9a0643c0d16b2e10fa6dee12c409f3b
YogeshTiwari10 <yogeshtiwari733@gmail.com>
2023-08-10 00:59:16 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import {default as logo} from './logo.png'
+import {default as walking_man} from './walking-man.gif'
+import {default as show_btn} from './three-dots.png'
+import {default as hide_btn} from './cross-button.png'
+
+export{
+    logo,
+    walking_man,
+    show_btn,
+    hide_btn,
+}
